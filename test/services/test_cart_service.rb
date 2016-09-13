5d6e8a7deb9a2755f2cc684b6514eb3db7b96c6e
Rohit Khatana <rohitkhatana.khatana@gmail.com>
2016-09-14 02:49:40 +0530

    added test cases for cart with fixtures and update README

--- test/services/test_cart_service.rb
+++ test/services/test_cart_service.rb
@@ -1,13 +1,47 @@
 require "minitest/autorun"
 require 'test_helper'
 class CartTest < ActiveSupport::TestCase
-  cart = Cart.new([{price: 200, quantity: 2, location: 'CA', description: 'cloth'}])
 
-  test "cart total without round" do
-    assert_equal cart.total, 438.8
+  test "should empty cart" do
+    cart = Cart.new([{price: 200, quantity: 2, state: 'CA',
+       description: 'cloth', category_id: 2}])
+    cart.clear
+    assert_equal cart.items, []
   end
 
-  test "cart total with round" do
-    assert_equal cart.rounded_total, 438.8
+  test "empty cart should have total 0" do
+    empty_cart = Cart.new []
+    assert_equal empty_cart.total, 0
   end
+
+  test "tax should not be applicable on food in California" do
+    cart = Cart.new([{price:200, quantity: 2, state: 'CA',
+       description: 'cloth', category_id: 1}])
+    assert_equal cart.rounded_total, 400
+  end
+
+  test "tax should not be applicable on food in New Jersey" do
+    cart = Cart.new([{price:200, quantity: 2, state: 'NJ',
+       description: 'cloth', category_id: 1}])
+    assert_equal cart.rounded_total, 400
+  end
+
+  test "tax should not be applicable on cloth in New Jersey" do
+    cart = Cart.new([{price:200, quantity: 2, state: 'NJ',
+       description: 'cloth', category_id: 2}])
+    assert_equal cart.rounded_total, 400
+  end
+
+  test "cart total with round should give rounded (nearest 0.05) amount" do
+    cart = Cart.new([{price: 14, quantity: 2, state: 'CA',
+       description: 'cloth', category_id: 2}])
+    assert_equal cart.rounded_total, 30.75
+  end
+
+  test "cart total without round should give actual amount" do
+    cart = Cart.new([{price: 14, quantity: 2, state: 'CA',
+       description: 'cloth', category_id: 2}])
+    assert_equal cart.total.to_f, 30.73
+  end
+
 end
